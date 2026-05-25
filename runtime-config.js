5d565b276ecfcc95a938419841730039d34cc599
cedocproject-admin <cedocproject@gmail.com>
2026-05-26 00:21:39 +0800

    update
    update

---
+++ runtime-config.js
@@ -0,0 +1,6 @@
+window.CEDOC_RUNTIME_CONFIG = {
+  "apiUrl": "",
+  "apiKey": "",
+  "supabaseUrl": "https://qkvfynzduderxkmdzeib.supabase.co",
+  "supabasePublishableKey": "sb_publishable_9uWcY-N8R8qBoU1pzGvchQ_buAGJci0"
+};
